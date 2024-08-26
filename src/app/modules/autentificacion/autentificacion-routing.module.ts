44472f07663a56d184e6c4f18e394a66df8b2714
floralocacciato <florencialocacciato@gmail.com>
2024-08-26 20:30:11 -0300

    inicio sesion component y sus rutas

--- src/app/modules/autentificacion/autentificacion-routing.module.ts
+++ src/app/modules/autentificacion/autentificacion-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// IMPORTACIÓN A LAS VISTAS DEL MÓDULO AUTENTIFICACIÓN
+import { RegistroComponent } from './pages/registro/registro.component';
+import { InicioSesionComponent } from './pages/inicio-sesion/inicio-sesion.component';
+
+const routes: Routes = [
+  {
+    path:"registro",component:RegistroComponent
+  },
+  {
+    path:"inicio-sesion",component:InicioSesionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/autentificacion/autentificacion.module.ts
+++ src/app/modules/autentificacion/autentificacion.module.ts
@@ -2,13 +2,46 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AutentificacionRoutingModule } from './autentificacion-routing.module';
+import { InicioSesionComponent } from './pages/inicio-sesion/inicio-sesion.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+
+// COMPONENTES DE MATERIAL
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
+// COMPONENTES DE ANGULAR
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InicioSesionComponent,
+    RegistroComponent
+  ],
   imports: [
     CommonModule,
-    AutentificacionRoutingModule
-  ]
+    AutentificacionRoutingModule,
+    //material
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule,
+    //angular
+    FormsModule
+  ],
+  exports: [
+    InicioSesionComponent,
+    RegistroComponent,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule,
+    FormsModule
+    ]
 })
 export class AutentificacionModule { }
